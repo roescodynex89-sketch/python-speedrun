6752d6dd1144572ab0490295b68b4b14bd7fbc09
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-09-08 20:30:32 +0600

    problem-1

---
+++ README.md
@@ -0,0 +1,8 @@
+Chapter 1 — Modules, Comments & pip
+
+-Comment: # its use fot comment
+-Module:  import math 
+ print (math.sqrt(25))
+
+pip: package install tool → pip install requests
+
---
+++ problem-1.py
@@ -0,0 +1,13 @@
+print('''
+High above the clouds so soft and bright,
+Two tiny stars shine through the night.
+With pastel hair and robes of white,
+They fill the sky with gentle light.
+Kiki flies with a star on his back,
+Finding adventure on every track.
+Lala wishes on her wand so grand,
+Sprinkling love across the land
+.From Dream Star-Cloud down to the Earth,
+They bring sweet smiles and happy mirth.
+Twin star angels,
+ hand in hand,The sweetest friends in all the land.''')
